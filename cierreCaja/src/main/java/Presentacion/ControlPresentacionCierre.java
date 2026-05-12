c0b945b4ad6b2ab788e36e46a60f426d396f976d
morirejo <ricardo.perez252951@potros.itson.edu.mx>
2026-05-11 19:34:52 -0700

    Add files via upload

---
+++ cierreCaja/src/main/java/Presentacion/ControlPresentacionCierre.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Presentacion;
+
+import dtos.cierreCaja.CorteCajaDTO;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import negocio.ControlNegocioCierre;
+
+/**
+ *
+ * @author MoriTejo
+ */
+public class ControlPresentacionCierre {
+    private ControlNegocioCierre negocio = new ControlNegocioCierre();
+    private CorteCajaDTO corteTemporal;
+
+    public void iniciarCierre() {
+        new PantallaCierrePrincipal(this).setVisible(true);
+    }
+
+    public void irAConteoEfectivo(JFrame actual) {
+        actual.dispose(); 
+        new PantallaConteoEfectivo(this).setVisible(true);
+    }
+
+    public void procesarConteo(double efectivoFisico, JFrame actual) {
+        this.corteTemporal = negocio.calcularCierre(efectivoFisico); 
+        actual.dispose();
+        new PantallaRevisionCorte(this, corteTemporal).setVisible(true);
+    }
+
+    public void irAReporteIncidente(JFrame actual) {
+        actual.dispose();
+        new PantallaReporteIncidente(this).setVisible(true);
+    }
+
+    public void finalizarCorte(JFrame actual) {
+        boolean exito = negocio.guardarCierre(this.corteTemporal);
+        
+        if (exito) {
+            actual.dispose();
+            new PantallaTurnoCerrado(this).setVisible(true);
+        } else {
+            JOptionPane.showMessageDialog(actual, "Error al guardar el corte en la base de datos.");
+        }
+    }
+}
